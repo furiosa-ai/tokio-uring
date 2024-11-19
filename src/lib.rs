18096b39adb73bbed84fdeea1ca35b9dde561f29
Sidong Yang <sidong.yang@furiosa.ai>
2024-11-19 20:16:59 +0900

    safe link

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,7 @@ pub mod fs;
 pub mod net;
 
 pub use io::write::*;
+pub use io::read::*;
 pub use runtime::driver::op::{InFlightOneshot, OneshotOutputTransform, UnsubmittedOneshot};
 pub use runtime::spawn;
 pub use runtime::Runtime;
